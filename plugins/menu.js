b8e03f0edf07417c4ec1e42c58851bd0786ac900
supuna <92195424+supuna28@users.noreply.github.com>
2021-12-25 21:38:05 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -8,7 +8,7 @@ let levelling = require('../lib/levelling')
 
 let tags = {
 
-  'main': 'MENU UTAMA',
+  'main': 'MENU ALL',
 
   'game': 'MENU GAME',
 
@@ -312,7 +312,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
 
            locationMessage: { 
 
-           jpegThumbnail: fs.readFileSync('./media/newyear.jpg') },
+           jpegThumbnail: fs.readFileSync('./media/menu.jpg') },
 
            hydratedFooterText: wm,
 
